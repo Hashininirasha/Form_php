c3c6b6723f0552d677dad80157cb5d04ad1ccc71
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-31 22:18:49 +0530

    Added form action

--- insert.php
+++ insert.php
@@ -6,12 +6,16 @@
     <title>Insert</title>
 </head>
 <body>
+    <form action ="insert.php" method="POST">
 
+
+
+    </form>
 
 
 </body>
 </html>
-<? php mysqli_close() ?>
+<? php mysqli_close($con); ?>
 
 
 

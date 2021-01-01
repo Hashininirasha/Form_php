f6a59170fe3c4745b0a192ca2e1a07b5c08ef159
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-01 11:01:12 +0530

    Added form details

--- insert.php
+++ insert.php
@@ -7,8 +7,11 @@
 </head>
 <body>
     <form action ="insert.php" method="POST">
-
-
+    First Name : <input type="text" name="first_name"><br>
+    Last Name : <input type="text" name="last_name"><br>
+    Phone : <input type ="text" name="phone"><br>
+    Pass Word : <input type="password" name="pw"><br>
+    <input type="submit" name="submit"><br>
 
     </form>
 
